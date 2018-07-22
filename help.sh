62f571e8217300686397760ea57a28e3bc8d2716
Christopher Hartmann <dev@chriha.de>
2018-07-22 02:58:07 +0200

    correct help comment

--- help.sh
+++ help.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # # # # # # # # # # # # # # # # # # # #
-# show docker commands help if local config file exists
-if [ -f "$WDIR/docker-compose.yml" ]; then
+# show ngrok commands help if local config file exists
+if [ -f "$PROJECT_CONFIG" ]; then
     printf "NGROK COMMANDS:\n"
     printf "    ${BLUE}expose${NORMAL}${HELP_SPACE:6}Temporarily expose the application (ngrok required).\n"
 fi
